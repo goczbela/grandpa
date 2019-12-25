2a728c5b2e99b116179cbe32c2c2d3ef279d8fc9
Gocz Bela <goczbela@gmail.com>
2019-12-25 23:33:31 +0100

    first commit - added the basic html,css,js files

---
+++ js/main.js
@@ -0,0 +1,51 @@
+/* COLOR CHOOSING */
+
+/*function setStylesheet(url) {
+  var stylesheet = document.getElementById("stylesheet");
+  stylesheet.setAttribute("href", url);
+}
+
+document.getElementById("homok-btn").addEventListener("click", myFunction);
+
+function myFunction() {
+  document.getElementById("bla").innerHTML = "faszomat";
+}
+
+
+var tengerbtn = document.getElementById("tenger-btn");
+
+tengerbtn.onClick = document.getElementById("bla").innerHTML = "Hello World";
+
+*/
+
+/*document.getElementById("homok-btn").addEventListener("click", function() {
+  var url = "css/homok.css";
+  var stylesheet = document.getElementById("stylesheet");
+  stylesheet.setAttribute("href", url);
+});
+
+document.getElementById("tenger-btn").addEventListener("click", function() {
+  var url = "css/tenger.css";
+  var stylesheet = document.getElementById("stylesheet");
+  stylesheet.setAttribute("href", url);
+});
+*/
+
+/*
+
+document.getElementById("homok-btn").addEventListener("click", function() {
+  setStylesheet(url);
+});
+
+document.getElementById("tenger-btn").addEventListener("click", function() {
+  setStylesheet(url);
+});
+
+*/
+
+/* PENSIONER CALCULATE FROM AGE */
+
+// onclick="setStylesheet('css/homok.css')
+// onclick="setStylesheet('css/tenger.css')"
+// onclick="setStylesheet('css/erdo.css')"
+// onclick="mehet()"
